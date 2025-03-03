eeecd9e12843c2301fc90eaa123a15ac7a0d1dff
Juan Salcedo <juandavidsalcedo85@gmail.com>
2025-03-02 23:31:36 -0500

    min 51:44

--- src/app/components/auth-button-server.tsx
+++ src/app/components/auth-button-server.tsx
@@ -1,7 +1,6 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers";
 import { AuthButtonClient } from "./auth-button-client";
-import {redirect} from "next/navigation";
 
 // logica del server
 export async function AuthButtonServer() {
